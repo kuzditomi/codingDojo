6fe31e89564969798571435bd1c4ecd7331785c5
yetipeti86 <yetipeti86@gmail.com>
2017-03-15 21:54:41 +0100

    New unit test project added for InputReaders

---
+++ Library/Library.InputReaders/NumberInputReaderTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Library.Helpers;
+using Moq;
+using Library.Contracts.PublicAPI.Helpers;
+
+namespace Library.InputReaders
+{
+    [TestFixture]
+    public class NumberInputReaderTests
+    {
+        private Mock<IInputReader<int>> _inputReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _inputReader = new Mock<IInputReader<int>>();
+        }
+
+        [Test]
+        public void NumberInputReader_ReceivesValidNumber_ReturnsTheNumber()
+        {
+            _inputReader.Setup(i=>i.)
+            var reader = new NumberInputReader();
+            reader.Read();
+        }
+    }
+}
