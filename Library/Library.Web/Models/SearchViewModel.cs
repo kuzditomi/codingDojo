f0380c90581fb8fc859fd60fbe44b42fc1753abc
Peter Komaromi <peter_komaromi@epam.com>
2017-04-24 13:50:48 +0200

    Homepage hits displayed

--- Library/Library.Web/Models/SearchViewModel.cs
+++ Library/Library.Web/Models/SearchViewModel.cs
@@ -3,7 +3,7 @@
     public class SearchViewModel
     {
         public string Title { get; set; }
-        //public string Author { get; set; }
-        //public int Year { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
     }
 }
